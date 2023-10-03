f31a7a58ec9b51fd186eab6ca5de7fcef6ca6473
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2023-10-03 02:46:18 -0400

    mobile fix

--- resources/js/Pages/ThreeOneOneModelList.vue
+++ resources/js/Pages/ThreeOneOneModelList.vue
@@ -11,52 +11,59 @@
       :key="key" 
       class="bg-gradient-to-r from-gray-100 via-gray-200 to-gray-100 p-6 rounded-lg mb-4 "
     >
-      <section>
+      <section class="">
           <h2 class="text-xl text-black font-extrabold">
             Model: <br /> {{ parseModelName(key) }}
           </h2>
         <div class="text-black text-opacity-80 m-10">
           Considering closed cases only:
         </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 p-4 bg-gray-100 rounded-lg shadow-md">
-  <div class="font-bold text-gray-700 text-lg md:text-xl">Top Predictions</div>
-  <div class="font-bold text-gray-700 text-lg md:text-xl">Performance Metrics</div>
-
-  <div class="text-gray-600 text-sm md:text-base flex items-center">
-    <span class="w-32">Most Likely Accuracy:</span>
-    <span class="ml-auto text-green-600">{{ value.firstaccuracy.toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2}) }}</span>
+        <div class="px-4 py-2 bg-gray-100 rounded-lg shadow-md">
+  <div class="font-bold text-xl mb-2">Model Predictions</div>
+  
+  <div class="flex flex-col md:flex-col">
+    <!-- Most Likely Prediction -->
+    <div class="flex flex-col w-full md:w-full mb-4 md:mb-4">
+      <span class="text-gray-700 font-semibold text-lg">Most Likely</span>
+      <div class="text-md text-gray-600 flex justify-between">
+        <span>Accuracy:</span>
+        <span class="text-green-600">{{ value.firstaccuracy.toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2}) }}</span>
+      </div>
+      <div class="text-md text-gray-600 flex justify-between">
+        <span>Correct Cases:</span>
+        <span>{{ value.firstcorrect }}</span>
+      </div>
+    </div>
+    
+    <!-- Next Likely Prediction -->
+    <div class="flex flex-col w-full md:w-auto">
+      <span class="text-gray-700 font-semibold text-lg">Next Likely</span>
+      <div class="text-md text-gray-600 flex justify-between">
+        <span>Accuracy:</span>
+        <span class="text-yellow-600">{{ value.secondaccuracy.toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2}) }}</span>
+      </div>
+      <div class="text-md text-gray-600 flex justify-between">
+        <span>Correct Cases:</span>
+        <span>{{ value.secondcorrect }}</span>
+      </div>
+    </div>
   </div>
-  <div class="text-gray-600 text-sm md:text-base flex items-center">
-    <span class="w-32">Most Likely Cases:</span>
-    <span class="ml-auto">{{ value.firstcorrect }}</span>
-  </div>
-
-  <div class="text-gray-600 text-sm md:text-base flex items-center">
-    <span class="w-32">Next Likely Accuracy:</span>
-    <span class="ml-auto text-yellow-600">{{ value.secondaccuracy.toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2}) }}</span>
-  </div>
-  <div class="text-gray-600 text-sm md:text-base flex items-center">
-    <span class="w-32">Next Likely Cases:</span>
-    <span class="ml-auto">{{ value.secondcorrect }}</span>
-  </div>
-
-  <div class="font-bold text-gray-700 text-lg md:text-xl">Composite Results</div>
-  <div class="font-bold text-gray-700 text-lg md:text-xl">Aggregate Metrics</div>
-
-  <div class="text-gray-600 text-sm md:text-base flex items-center">
-    <span class="w-32">Combined Accuracy:</span>
-    <span class="ml-auto text-blue-600">{{ (value.secondaccuracy+value.firstaccuracy).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2}) }}</span>
-  </div>
-  <div class="text-gray-600 text-sm md:text-base flex items-center">
-    <span class="w-32">Total Correct Predictions:</span>
-    <span class="ml-auto">{{ value.secondcorrect + value.firstcorrect }}</span>
-  </div>
-
-  <div class="col-span-2 font-bold text-gray-700 text-lg md:text-xl">Case Statistics</div>
-
-  <div class="col-span-2 text-gray-600 text-sm md:text-base flex items-center">
-    <span class="w-32">Total Cases Analyzed:</span>
-    <span class="ml-auto">{{ value.total }}</span>
+  
+  <!-- Aggregate Metrics -->
+  <div class="flex flex-col mt-4">
+    <span class="text-gray-700 font-semibold text-lg mb-2">Aggregate Metrics</span>
+    <div class="text-md text-gray-600 flex justify-between">
+      <span>Combined Accuracy:</span>
+      <span class="text-blue-600">{{ (value.secondaccuracy + value.firstaccuracy).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2}) }}</span>
+    </div>
+    <div class="text-md text-gray-600 flex justify-between">
+      <span>Total Correct:</span>
+      <span>{{ value.secondcorrect + value.firstcorrect }}</span>
+    </div>
+    <div class="text-md text-gray-600 flex justify-between">
+      <span>Total Cases:</span>
+      <span>{{ value.total }}</span>
+    </div>
   </div>
 </div>
 
@@ -100,7 +107,9 @@
             const minute = time.slice(2,4);
             const second = time.slice(4,6);
             const name = modelName.split('_')[2];
-            return `${year}-${month}-${day} ${hour}:${minute}:${second} ${name}`;
+            //add spaces between letters and numbers
+            const namespace = name.replace(/([0-9])([a-z])/ig, '$1 $2');
+            return `${year}-${month}-${day} ${hour}:${minute}:${second} ${namespace}`;
         },
     }
   };
