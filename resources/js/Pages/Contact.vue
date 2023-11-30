cc0c2741816df9e15246dd1f95dcebaacebfdcc3
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2023-11-30 13:36:24 -0500

    update contact

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -29,8 +29,8 @@ defineProps({
         </Head>
     <main>
       <section>
-        <h2>Contact Page Coming Soon!</h2>
-        <p>This page is under construction, thank you for your interest!</p>
+        <h2>Connect with us on GitHub</h2>
+        <p><a href="https://github.com/mindfulcoder49/" target="_blank" class="btn btn-primary">This is the GitHub</a> for the Boston Open Data Collaborative for Diversity, Equity, and Inclusion. All of our code is open source. Put in an issue and we will get back to you!</p>
         <!-- <button>View Details</button> -->
       </section>
     </main>
