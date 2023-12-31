0bdfe3e3397cf5bee748cd3247394d640ac94d6e
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2023-12-30 22:31:44 -0500

    New Design for 2024

--- resources/js/Components/PageTemplate.vue
+++ resources/js/Components/PageTemplate.vue
@@ -1,43 +1,43 @@
 <script>
 import { Head, Link } from '@inertiajs/vue3';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
 
 export default {
   name: 'PageTemplate',
   components: {
     Link,
     Head,
+    GuestLayout,
   },
 };
 </script>
 
 <template>
-    <!DOCTYPE html>
- <html lang="en">
-   <Head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1" />
-   </Head>
-   <body class="pageTemplate">
-     <header>
-     <div class="smaller-square"><div class="larger-square"></div></div>
-       <h1>BODC-DEI</h1>
-       <nav>
-         <ul>
-            <li><Link :href="route('home')" class="btn btn-primary">Home</Link></li>
-            <li><Link :href="route('about')" class="btn btn-primary">About</Link></li>
-            <li><Link :href="route('projects')" class="btn btn-primary">Projects</Link></li>
-            <li><Link :href="route('contact')" class="btn btn-primary">Contact</Link></li>
-         </ul>
-       </nav>
-     </header>
+<GuestLayout>
+
+  <header class="bg-white p-4">
+  <div class="container mx-auto flex justify-between items-center">
+    <h1 class="text-2xl font-semibold">BODC-DEI</h1>
+    <nav>
+      <ul class="flex space-x-4">
+        <li><Link :href="route('home')" class="text-base text-gray-700 hover:text-gray-900">Home</Link></li>
+        <li><Link :href="route('about')" class="text-base text-gray-700 hover:text-gray-900">About</Link></li>
+        <li><Link :href="route('projects')" class="text-base text-gray-700 hover:text-gray-900">Projects</Link></li>
+        <li><Link :href="route('contact')" class="text-base text-gray-700 hover:text-gray-900">Contact</Link></li>
+        <!--Add login and register links-->
+        <li><Link :href="route('login')" class="text-base text-gray-700 hover:text-gray-900">Login</Link></li>
+        <li><Link :href="route('register')" class="text-base text-gray-700 hover:text-gray-900">Register</Link></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
      <slot></slot>
   
      <footer>
-        <div class="larger-square"><div class="smaller-square"></div></div>
        <p>&copy; 2023 BODC-DEI</p>
      </footer>
-   </body>
- </html>
+    </GuestLayout>
  </template>
   
 
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
 import PageTemplate from '../Components/PageTemplate.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
 
 defineProps({
     canLogin: {
@@ -21,36 +23,28 @@ defineProps({
 </script>
 
 <template>
-
-<PageTemplate>
-  <Head>
-    <title>Home</title>
-  </Head>
-    <main>
-    
-      <section>
-        <h2>Welcome to BODC-DEI</h2>
-      </section>  
-      <section>
-        <h2>Coming Soon: Boston 311 Project</h2>
-        <p>The Boston 311 project is a machine learning project focused on the 311 service requests dataset, which is available on data.boston.gov. The project aims to develop a tool that can predict the likelihood of a 311 service request being resolved within a certain timeframe, based on various factors such as the type of request, the location of the request, and the time of day.</p>
-
-        <p>By analyzing the data from the 311 service requests, the project team hopes to gain insights into the patterns and trends of service requests in the city of Boston. The ultimate goal is to develop a tool that can assist city officials in allocating resources and prioritizing service requests more effectively.</p>
-          
-        <p>The team is using Jupyter notebooks and Google Colaboratory to analyze the data and develop predictive models, and they are planning to build a web application to showcase their findings.</p>
-
-        <p>See the code demo here:</p>
-        <p><Link href="/311demo" class="btn btn-primary">This page has a Jupyter notebook showing a demo of how the Boston 311 python package can easily be used to train several types of predictive models, including a Neural Network.</Link> </p>
-        <!-- <button>View Details</button> -->
-      </section>
-      <section>
-        <h2>Coming Soon: "The Boston App"</h2>
-        <p>The proposed project is an app for residents of Boston to stay informed about city operations in their area that affect their daily lives. The app will provide information about city services, such as trash pickup and street cleaning, as well street construction, building permits, and health inspections</p>
-        <p>See the code demo here:</p>
-        <p><Link href="/thebostonappdemo" class="btn btn-primary">This page shows examples of the kind of reports this app might generate, and a python notebook with proof of concept code that generates those example reports.</Link></p>
-        <!-- <button>View Details</button> -->
-      </section>
-    </main>
-</PageTemplate>
-
-</template>
+    <PageTemplate>
+      <Head>
+        <title class="text-xl font-bold">Home</title>
+      </Head>
+      <main class="p-4">
+        <section class="mb-8">
+          <h2 class="text-2xl font-semibold mb-3">Welcome to BODC-DEI</h2>
+        </section>  
+        <section class="mb-8">
+          <h2 class="text-2xl font-semibold mb-3">Coming Soon: Boston 311 Project</h2>
+          <p class="text-base mb-4">The Boston 311 project is a machine learning project focused on the 311 service requests dataset, which is available on data.boston.gov. The project aims to develop a tool that can predict the likelihood of a 311 service request being resolved within a certain timeframe, based on various factors such as the type of request, the location of the request, and the time of day.</p>
+          <!-- Additional paragraphs -->
+          <p class="text-base mb-4">See the code demo here:</p>
+          <Link href="/311demo" class="btn btn-primary">View Demo</Link>
+        </section>
+        <section>
+          <h2 class="text-2xl font-semibold mb-3">Coming Soon: "The Boston App"</h2>
+          <p class="text-base mb-4">The proposed project is an app for residents of Boston to stay informed about city operations in their area that affect their daily lives. The app will provide information about city services, such as trash pickup and street cleaning, as well street construction, building permits, and health inspections.</p>
+          <p class="text-base mb-4">See the code demo here:</p>
+          <Link href="/thebostonappdemo" class="btn btn-primary">View Demo</Link>
+        </section>
+      </main>
+    </PageTemplate>
+  </template>
+  
